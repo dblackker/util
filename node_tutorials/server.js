75f2cde3a33bb917bdffdfdeb79395c993334ad2
Lukas Gemar <lukasgemar@gmail.com>
2014-10-31 16:33:04 -0400

    Added awesome chat example

--- node_tutorials/server.js
+++ node_tutorials/server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var s = http.createServer(function(req, res) {
 			res.writeHead(200, { 'content-type':'text/plain' });
-			res.end("hello world\n");
+			res.write("hello\n");
+			setTimeout(function() {
+				res.end("world\n");
+			}, 5000);
 			console.log("I served up a juicy request!");
 		});
 s.listen(8000);
